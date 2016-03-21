db6b0f161a854a8adfd451282a9f3e61937eb470
Ömer Faruk Görmel <gormelomerfaruk@gmail.com>
2016-03-21 15:14:52 +0200

    first commit

---
+++ app1/app1.c
@@ -0,0 +1,57 @@
+// Sistem Programlama
+// Soru 1
+// Dosya adı: app1.c
+// @gormelof
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main fonksiyonu standardında iki
+ * parametre vardır:
+ * argc: konsolda programı çalıştırırken
+ * parametrelerin sayısını tutar 
+ * (program adınıda parametre olarak görür.)
+ * argv: bir dizi işaretcisidir. Yine konsolda
+ * program çalıştıtırken kullandığımız parametreleri
+ * tutar.
+*/
+
+int main(int argc, char *argv[]) {
+
+	char hex[50]; // degeğerleri hexadecimal'e dönüşrürülürken kullanılacak dizi.
+
+	int N = atoi(argv[1]); // char olarak gelen main parametresini (konsoldan gelen) integer' a dönültürür ve dizi boyutu yapar.
+
+	int i; // döngü sayacı
+
+	unsigned long dizi[N]; // ilk dizi
+	unsigned char yeni_dizi[N]; // dizinin dönüşüm yapılmış halini tutacak yeni dizi
+
+	srand(time(NULL)); // random sayı üretmek için kullandığımız dizi
+
+	// dizi elemanlarını random atayıp ekrana gerekli formatlarda bastığımız döngü
+	for(int i = 0; i < N; i++) {
+		dizi[i] = rand(); // diziye random değerler atınıyor
+
+		printf("Elemanin degeri             -> dizi[%d]  = %lu\n", i, dizi[i]);
+		sprintf(hex, "%lx", dizi[i]); // dizi elemanının hexadecimal değere dönüşümü
+		printf("Elemanin hexadecimal degeri -> dizi[%d]  = 0x%s\n", i, hex);
+		printf("Elemanin adresi             -> &dizi[%d] = %p\n", i, &dizi[i]);
+	}
+
+	printf("\n--------------------------------------------------\n\n");
+
+	// type casting işlemi yaptığımız ve yeni değerlerin basıldığı döngü
+	for(int i = 0; i < N; i++) {
+		yeni_dizi[i] = (unsigned char) dizi[i]; // type casting
+
+		printf("Elemanin degeri             -> yeni_dizi[%d]  = %u\n", i, yeni_dizi[i]);
+		sprintf(hex, "%x", yeni_dizi[i]); // dizi elemanının hexadecimal değere dönüşümü
+		printf("Elemanin hexadecimal degeri -> yeni_dizi[%d]  = 0x%s\n", i, hex);
+		printf("Elemanin adresi             -> &yeni_dizi[%d] = %p\n", i, &yeni_dizi[i]);
+	}
+
+	return 0;
+}
---
+++ app2/app2.c
@@ -0,0 +1,58 @@
+// Sistem Programlama
+// Soru 2
+// Dosya adı: app2.c
+// @gormelof
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+
+/**
+ * yine main fonksiyonu parametrelerinden(argc, argv) faydalanıyoruz.
+ * bu sefer komut satırı için option(seçenek) belirleyerek işlemler yapılıyor.
+ * getopt() fonksiyonu bu programda anahtar fonksiyon (kilit oyuncu, başrol, vs.)
+*/
+int main(int argc, char *argv[]) {
+
+	int t_flag = 0, c_flag = 0; // bu flag'leri seçenekleri belirlemek için tanımlıyoruz
+	int ch, i; // döngü değişkenleri
+
+	// -t, -c gibi parametreleri bu döngü içerisinde belirliyoruz.
+	while((ch = getopt(argc, argv, "t:c:")) != -1) {
+		switch(ch) {
+			case 't':
+			printf("t secenegi seçildi\n");
+			t_flag = 1; // -t seçeneği için t_flag aktif oldu
+			// t_arg = optarg;
+			break;
+			case 'c':
+			printf("c secenegi secildi\n");
+			c_flag = 1; // -c seçeneği için c_flag aktif oldu
+			// c_arg = optarg;
+			break;
+		}
+	}
+
+	// toplam için etkisiz eleman = 0
+	// çarpım için etkisiz eleman = 1
+	int toplam = 0, carpim = 1;
+	
+	if(argc == 1) { // tek parametre program adıdır, bu bizim işimize yaramaz yanlış kullanım
+		printf("Gecersiz giris. Dogru kullanım orenegi: \"./program -c 1 2 3 4\" \n");
+	} else if(t_flag == 1 && c_flag == 0) { // -t'nin aktif olduğu kısım
+		for(int i = 2; i < argc; i++) {
+			toplam += atoi(argv[i]); // parametreler atoi() fonksiyonu ile int dönüşümü yapılarak işleme sokuluyor
+		}
+		printf("Toplam = %d\n", toplam); // toplam sonucu ekrana basılıyor
+	} else if(t_flag == 0 && c_flag == 1) { // -c'nin aktif olduğu kısım
+		for(int i = 2; i < argc; i++) {
+			carpim *= atoi(argv[i]); // parametreler atoi() fonksiyonu ile int dönüşümü yapılarak işleme sokuluyor
+		}
+		printf("Carpim = %d\n", carpim); // çarpım sonucu ekrana basılıyor
+	} else {
+		printf("Gecersiz islem!\n"); // seçenek belirlenemedi o zaman işlemde yok gardaş :)
+	}
+
+	return 0;
+}
